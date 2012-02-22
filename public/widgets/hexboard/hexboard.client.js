d4561391ff42cbfd6b61ed1325ad00d9c0315499
jflegle <jason.flegle@zyquest.com>
2012-02-22 11:02:26 -0600

    first pass

--- public/widgets/hexboard/hexboard.client.js
+++ public/widgets/hexboard/hexboard.client.js
@@ -1,15 +1,115 @@
 feather.ns("catchakitty");
 (function() {
+  var me = 0;
   catchakitty.hexboard = feather.Widget.create({
     name: "catchakitty.hexboard",
     path: "widgets/hexboard/",
     prototype: {
-      onInit: function() {
+      onInit: function( ) {
         
       },
-      onReady: function() {
+      onReady: function( ) {
+        me = this;
+        this.width = 0;
+        this.height = 0;
+        this.totalTiles = 0;
+        this.grid = 0;
+        this.tileWidgets = 0;
+      },
+      _onCreateTile: function( callback ) {
         
-      }
+        feather.Widget.load( { // creats a widget
+          path: "widgets/hextile/", // path to widget
+          clientOptions: {
+            parent: me,
+            keepContainerOnDispose: true,
+            container: $("<div class='gameBoard'></div>").prependTo( me.get( "#gameBoard" ) ), // adding the html
+            onceState: {
+              ready: function( ) {
+                callback( this );
+              }
+            }
+          }
+        });
+
+      },
+      _onCreateTileCallback: function( obj ) {
+        me.tileWidgets[me.tileWidgets.length] = obj;
+
+        if( me.tileWidgets.length >= me.totalTiles ){
+          me._onCreateBoardDone( );
+        }
+      },
+      _onCreateBoardDone: function( ) {
+        var index = 0;
+        var xOffset = 0;
+        var yOffset = 0;
+        var width = this.tileWidgets[0].onGetWidth( );
+        var halfHeight = this.tileWidgets[0].onGetHeight( )/2;
+        for( var y = 0; y < this.height; y++ ) {            
+          for( var x = 0; x < this.width; x++ ) {
+              this.grid[x][y] = this.tileWidgets[index];
+              if( x%2 ){
+                // odd columns
+                this.tileWidgets[index].onSetLink(  (y != 0)? this.grid[x][y-1]:null,  // north
+                                                    (y != this.height-1)? this.grid[x][y+1]:null,  // south
+                                                    (x != this.width-1)? this.grid[x+1][y]:null,  // north east
+                                                    (x != 0)? this.grid[x-1][y]:null,  // north west
+                                                    (x != this.width-1 && y != this.height-1)? this.grid[x+1][y+1]:null,// south east
+                                                    (x != 0)? this.grid[x-1][y+1]:null);// south west
+              }
+              else {
+                // even columns
+                this.tileWidgets[index].onSetLink(  (y != 0)? this.grid[x][y-1]:null,  // north
+                                                    (y != this.height-1)? this.grid[x][y+1]:null,  // south
+                                                    (x != this.width-1 && y != 0)? this.grid[x+1][y-1]:null,// north east
+                                                    (y != 0 && x != 0)? this.grid[x-1][y-1]:null,// north west
+                                                    (x != this.width-1)? this.grid[x+1][y]:null,  // south east
+                                                    (x != 0)? this.grid[x-1][y]:null);// south west
+              }
+              this.tileWidgets[index++].onSetTile( xOffset, yOffset );
+              xOffset += width;
+              yOffset += (x%2)? -halfHeight:halfHeight;
+          }
+          yOffset += halfHeight;
+          xOffset = 0;
+        }
+        
+        //this.grid[2][0].get("#hexTileImage").addClass( "blocked");
+        /*this.grid[1][2].get("#hexTileImage").addClass( "blocked");
+        this.grid[0][2].get("#hexTileImage").addClass( "blocked");
+        this.grid[2][2].north.north.get("#hexTileImage").addClass( "blocked");*/
+        /*this.grid[2][2].nwest.nwest.get("#hexTileImage").addClass( "blocked");
+        this.grid[2][2].neast.neast.get("#hexTileImage").addClass( "blocked");
+        this.grid[2][2].south.south.get("#hexTileImage").addClass( "blocked");
+        this.grid[2][2].north.north.get("#hexTileImage").addClass( "blocked");        
+        this.grid[2][2].swest.swest.get("#hexTileImage").addClass( "blocked");
+        this.grid[2][2].seast.seast.get("#hexTileImage").addClass( "blocked");*/
+        //alert( this.grid[2][0].north == null );
+
+        me.fire('LoadingDone');
+        
+      },
+      onCreateBoard: function( width, height, kitty ) {
+        this.width = width;
+        this.height = height;
+        this.totalTiles = width * height;
+        this.kitty = kitty;
+               
+        // create the grid
+        this.grid = [width];
+        for( var i = 0; i < width; i++ ) {
+         this.grid[i] = [];
+        }
+
+        // create tiles
+        if( this.tileWidgets != 0 ) { alert("board already created");}
+        this.tileWidgets = [];
+        for( var i = 0; i < this.totalTiles; i++ ) {
+          this._onCreateTile( this._onCreateTileCallback );
+        }
+
+      },
     }
   });
 })();
--- public/widgets/hextile/hextile.client.js
+++ public/widgets/hextile/hextile.client.js
@@ -4,12 +4,51 @@ feather.ns("catchakitty");
     name: "catchakitty.hextile",
     path: "widgets/hextile/",
     prototype: {
-      onInit: function() {
+      onInit: function( ) {
         
       },
-      onReady: function() {
+      onReady: function( ) {
+        var me = this;
+        this.north = null; // might want directions in array form
+        this.south = null;
+        this.neast = null;
+        this.seast = null;
+        this.nwest = null;
+        this.swest = null;
+
+        me.domEvents.bind( me.get("#hexTileImage"), "mouseenter", function( ) { 
+          me.get("#hexTileImage").addClass("over");
+        });
+
+        me.domEvents.bind( me.get("#hexTileImage"), "mouseout", function( ) {
+          me.get("#hexTileImage").removeClass("over");
+        });
+
+        me.domEvents.bind( me.get("#hexTileImage"), "click", function( ) {
+          me.get("#hexTileImage").addClass("blocked"); 
+        });
         
-      }
+      },
+      onSetTile: function( posX, posY ) {
+        var image = this.get("#hexTileImage");
+
+        image.css("left", posX);
+        image.css("top", posY);
+      },
+      onSetLink: function( n, s, ne, nw, se, sw ) {
+        if( n != null ) { this.north = n; n.south = this; }
+        if( s != null ) { this.south = s; s.north = this; }
+        if( ne != null ) { this.neast = ne; ne.swest = this; }
+        if( nw != null ) { this.nwest = nw; nw.seast = this; }
+        if( se != null ) { this.seast = se; se.nwest = this; }
+        if( sw != null ) { this.swest = sw; sw.neast = this; }
+      },
+      onGetWidth: function( ) {
+        return 64;
+      },
+      onGetHeight: function( ) {
+        return 64;
+      },
     }
   });
 })();
